507b5a2ee51a59bc228ef34c53b059c316261da1
Iuri <bentoiuri@gmail.com>
2025-05-09 15:47:07 -0300

    feat: create model and dtos

---
+++ src/app/core/model/Sale.ts
@@ -0,0 +1,17 @@
+import { Product } from "./Product";
+
+export class Sale{
+    id?: number;
+    client: string;
+    quantity: number;
+    totalPrice: number;
+    product: Product;
+
+    constructor(id: number, client: string, quantity: number, totalPrice: number, product: Product) {
+        this.id = id;
+        this.client = client;
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+        this.product = product;
+    }
+}
